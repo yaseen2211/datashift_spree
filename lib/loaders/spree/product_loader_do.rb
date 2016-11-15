8c5bc4ffd7fb0ba3d25fdd4b43fef748efac4b80
Pikender Sharma <pikender@vinsol.com>
2016-11-15 21:16:50 +0530

    Added images to product load

--- lib/loaders/spree/product_loader_do.rb
+++ lib/loaders/spree/product_loader_do.rb
@@ -12,12 +12,15 @@ module DataShift
   module SpreeEcom
     class ShopifyProductBinder < Binder
       def forced_inclusion_columns
-        extra_cols = ["images","variant_sku","variant_cost_price","variant_price","variant_images","stock_items", "price"]
+        extra_cols = ["images"]
         super() + extra_cols
       end
     end
 
     class ShopifyProductLoader < CsvLoader
+
+      include DataShift::ImageLoading
+
       def initialize
         @@image_klass ||= DataShift::SpreeEcom::get_spree_class('Image')
         @@option_type_klass ||= DataShift::SpreeEcom::get_spree_class('OptionType')
@@ -32,6 +35,8 @@ module DataShift
         @@variant_klass ||= DataShift::SpreeEcom::get_spree_class('Variant')
 
         super
+
+        @binder = ShopifyProductBinder.new
       end
 
 			def perform_load( _options = {} )
@@ -93,6 +98,8 @@ module DataShift
                     add_properties(value, doc_context, binder)
                   elsif(value && model_method.operator?('stock_items'))
                     add_variants_stock(value, doc_context, binder)
+                  elsif(value && model_method.operator?('images'))
+                    add_images(value, doc_context, binder)
                   else
                     context.process
                   end
@@ -442,6 +449,129 @@ module DataShift
             #puts "DEBUG Load Object now has Variants : #{load_object.variants.inspect}" if(verbose)
           end
         end # each Variant
+
+        # TOFIX - why is this in the base class when it looks like tis Prod/Vars ?
+        # either move it or make it generic so the owner can be any model that supports attachments
+
+        # Special case for Images
+        #
+        # A list of entries for Images.
+        #
+        # Multiple image items can be delimited by binder.multi_assoc_delim
+        #
+        # Each item can  contain optional attributes for the Image class within {}. 
+        # 
+        # For example to supply the optional 'alt' text, or position for an image
+        #
+        #   Example => path_1{:alt => text}|path_2{:alt => more alt blah blah, :position => 5}|path_3{:alt => the alt text for this path}
+        #
+        def add_images(value, doc_context, binder)
+          doc_context.save_if_new
+          load_object = doc_context.load_object
+
+          # different versions have moved images around from Prod to Variant
+          owner = DataShift::SpreeEcom::get_image_owner(load_object)
+
+          all_images = get_each_assoc(value, binder)
+
+          # multiple files should maintain comma separated logic with 'binder.multi_value_delim' and not 'binder.multi_assoc_delim'
+          all_images.each do |image|
+
+            #TODO - make this binder.attributes_start_delim and support {alt=> 'blah, :position => 2 etc}
+
+            # Test and code for this saved at : http://www.rubular.com/r/1de2TZsVJz
+
+            @spree_uri_regexp ||= Regexp::new('(http|ftp|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:\/~\+#]*[\w\-\@?^=%&amp;\/~\+#])?' )
+
+            if(image.match(@spree_uri_regexp))
+
+              uri, attributes = image.split(binder.attribute_list_start)
+
+              uri.strip!
+
+              logger.info("Processing IMAGE from URI [#{uri.inspect}]")
+
+              if(attributes)
+                #TODO move to ColumnPacker unpack ?
+                attributes = attributes.split(', ').map{|h| h1,h2 = h.split('=>'); {h1.strip! => h2.strip!}}.reduce(:merge)
+                logger.debug("IMAGE has additional attributes #{attributes.inspect}")
+              else
+                attributes = {} # will blow things up later if we pass nil where {} expected
+              end
+
+              agent = Mechanize.new
+
+              image = begin
+                agent.get(uri)
+              rescue => e
+                puts "ERROR: Failed to fetch image from URL #{uri}", e.message
+                raise DataShift::BadUri.new("Failed to fetch image from URL #{uri}")
+              end
+
+              # Expected image is_a Mechanize::Image
+              # image.filename& image.extract_filename do not handle query string well e,g blah.jpg?v=1234
+              # so for now use URI
+              # extname = image.respond_to?(:filename) ? File.extname(image.filename) : File.extname(uri)
+              extname =  File.extname( uri.gsub(/\?.*=.*/, ''))
+
+              base = image.respond_to?(:filename) ? File.basename(image.filename, '.*') : File.basename(uri, '.*')
+
+              logger.debug("Storing Image in TempFile #{base.inspect}.#{extname.inspect}")
+
+              @current_image_temp_file = Tempfile.new([base, extname], :encoding => 'ascii-8bit')
+
+              begin
+
+                # TODO can we handle embedded img src e.g from Mechanize::Page::Image ?
+
+                # If I call image.save(@current_image_temp_file.path) then it creates a new file with a .1 extension
+                # so the real temp file data is empty and paperclip chokes
+                # so this is a copy from the Mechanize::Image save method.  don't like it much, very brittle, but what to do ...
+                until image.body_io.eof? do
+                  @current_image_temp_file.write image.body_io.read 16384
+                end
+
+                @current_image_temp_file.rewind
+
+                logger.info("IMAGE downloaded from URI #{uri.inspect}")
+
+                attachment = create_attachment(@@image_klass, @current_image_temp_file.path, nil, nil, attributes)
+
+              rescue => e
+                logger.error(e.message)
+                logger.error("Failed to create Image from URL #{uri}")
+                raise DataShift::DataProcessingError.new("Failed to create Image from URL #{uri}")
+
+              ensure
+                @current_image_temp_file.close
+                @current_image_temp_file.unlink
+              end
+
+            else
+
+              path, alt_text = image.split(binder.name_value_delim)
+
+              logger.debug("Processing IMAGE from PATH #{path.inspect} #{alt_text.inspect}")
+
+              path = File.join(config[:image_path_prefix], path) if(config[:image_path_prefix])
+
+              attachment = create_attachment(@@image_klass, path, nil, nil, :alt => alt_text)
+            end
+
+            begin
+              owner.images << attachment
+
+              logger.debug("Product assigned Image from : #{path.inspect}")
+            rescue => e
+              puts "ERROR - Failed to assign attachment to #{owner.class} #{owner.id}"
+              logger.error("Failed to assign attachment to #{owner.class} #{owner.id}")
+            end
+
+          end
+
+          load_object.save
+
+        end
     end
   end
 end
